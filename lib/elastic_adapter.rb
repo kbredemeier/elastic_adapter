d4e2f5b66c4113378d5eea4b92e7bceb48553db3
Kristopher Bredemeier <kb@i22.de>
2015-01-10 12:49:30 +0100

    Added reset method to ElasticAdapter to restore the default configuration in tests

--- lib/elastic_adapter.rb
+++ lib/elastic_adapter.rb
@@ -15,6 +15,12 @@ module ElasticAdapter
       yield(configuration)
     end
 
+    # This method's primary purpose is to reset the
+    # Configufation in tests
+    def reset
+      @configuration = Configuration.new
+    end
+
     def configuration
       @configuration ||= Configuration.new
     end
--- spec/elastic_adapter_spec.rb
+++ spec/elastic_adapter_spec.rb
@@ -2,23 +2,33 @@ require "spec_helper"
 
 module ElasticAdapter
   describe "#configure" do
+    before :each do
+      ElasticAdapter.configure do |config|
+        config.url = "http://test:9999"
+      end
+    end
+
+    after :each do
+      ElasticAdapter.reset
+    end
+
+    it "returns the configured url" do
+      repo = Repository.new
+      expect(repo.url).to eq "http://test:9999"
+    end
+  end
+
+  describe "reset" do
     before do
       ElasticAdapter.configure do |config|
         config.url = "http://test:9999"
       end
     end
 
-    after do
-      # Configure back to default.
-      # Otherwise some specs might fail.
-      ElasticAdapter.configure do |config|
-        config.url = "http://localhost:9200"
-      end
-    end
-
-    it "returns the configured url" do
-      repo = Repository.new
-      expect(repo.url).to eq "http://test:9999"
+    it "resets the configuration" do
+      ElasticAdapter.reset
+      config = ElasticAdapter.configuration
+      expect(config.url).to eq "http://localhost:9200"
     end
   end
 end
