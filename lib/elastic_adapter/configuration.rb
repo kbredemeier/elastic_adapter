fb12f35ae0eed8a16b2af35f42ed05dda884b68a
Kristopher Bredemeier <kb@i22.de>
2015-01-12 12:41:47 +0100

    Implemented some behavior on Configuration class

--- lib/elastic_adapter/configuration.rb
+++ lib/elastic_adapter/configuration.rb
@@ -1,11 +1,39 @@
 module ElasticAdapter
   class Configuration
-    attr_accessor :log, :url
+    include ElasticAdapter::AttributeAccessor
+
+    attribute_reader :log, :url, :index_name, :wrapper, :settings, :mappings, :document_type
 
     def initialize(options = {})
-      @log = options.fetch(:log, true)
-      @url = options.fetch(:url, "http://localhost:9200")
+      @log = options.fetch :log do
+        ENV['ELASTIC_ADAPTER_LOG'] || true
+      end
+
+      @url = options.fetch :url do
+        ENV['ELASTIC_ADAPTER_URL'] || "http://localhost:9200"
+      end
+
+      @document_type = options.fetch(:document_type)
+      @index_name = options.fetch(:index_name)
+      @wrapper = options.fetch(:wrapper, OpenStruct)
+      @settings = options.fetch(:settings)
+      @mappings = options.fetch(:mappings)
     end
 
+    def to_hash
+      @options_hash ||= options_hash
+    end
+
+    private
+
+    def options_hash
+      hash = {}
+
+      attributes.each do |attr|
+        hash[attr] = self.send(attr)
+      end
+
+      hash
+    end
   end
 end
--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -2,45 +2,67 @@ require "spec_helper"
 
 module ElasticAdapter
   describe Configuration do
-    describe "attributes" do
-      before :each do
-        @config = Configuration.new
+    describe "#to_hash" do
+      let(:options) do
+        {
+          url: "url",
+          log: "log",
+          document_type: "document_type",
+          index_name: "index_name",
+          wrapper: "wrapper",
+          settings: "settings",
+          mappings: "mappings"
+        }
       end
 
-      describe "url" do
-        it "default value is http://localhost:9200" do
-          expect(@config.url).to eq "http://localhost:9200"
-        end
+      let(:subject) { Configuration.new(options) }
 
-        it "can set the value" do
-          @config.url= "test"
-          expect(@config.url).to eq "test"
-        end
-      end
-
-      describe "log" do
-        it "default value is true" do
-          expect(@config.log).to be true
-        end
-
-        it "can set the value" do
-          @config.log = false
-          expect(@config.log).to be false
-        end
+      it "returns the options as a hash" do
+        expect(subject.to_hash).to eq options
       end
     end
 
     describe "#initialize" do
-      it "accepts a hash" do
-        expect{ Configuration.new({}) }.not_to raise_error
+      let(:options) do
+        {
+          url: "url",
+          log: "log",
+          document_type: "document_type",
+          index_name: "index_name",
+          wrapper: "wrapper",
+          settings: "settings",
+          mappings: "mappings"
+        }
       end
 
+      let(:subject) { Configuration.new(options) }
+
       it "assigns the url" do
-        expect(Configuration.new({url: "test"}).url).to eq "test"
+        expect(subject.url).to eq "url"
       end
 
       it "assigns log" do
-        expect(Configuration.new({log: false}).log).to be false
+        expect(subject.log).to eq "log"
+      end
+
+      it "assigns index_name" do
+        expect(subject.index_name).to eq "index_name"
+      end
+
+      it "assigns wrapper" do
+        expect(subject.wrapper).to eq "wrapper"
+      end
+
+      it "assigns settings" do
+        expect(subject.settings).to eq "settings"
+      end
+
+      it "assigns mappings" do
+        expect(subject.mappings).to eq "mappings"
+      end
+
+      it "assigns document_type" do
+        expect(subject.document_type).to eq "document_type"
       end
     end
   end
