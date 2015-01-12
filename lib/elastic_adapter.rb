6d269c894ec6794c55b2e1619862b264fadefd5b
Kristopher Bredemeier <kb@i22.de>
2015-01-12 12:40:31 +0100

    Added AttributeAccessor module

--- lib/elastic_adapter.rb
+++ lib/elastic_adapter.rb
@@ -2,6 +2,7 @@ require "elasticsearch/transport"
 require "elasticsearch/api"
 
 require "elastic_adapter/version"
+require "elastic_adapter/attribute_accessor"
 require "elastic_adapter/configuration"
 require "elastic_adapter/repository"
 
