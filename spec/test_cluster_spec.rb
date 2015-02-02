846da1e8cf367fa2af120119ea6c453711fadb35
Kristopher Bredemeier <k.bredemeier@googlemail.com>
2015-02-02 22:47:42 +0100

    Removed spec for running elasticsearch

--- spec/test_cluster_spec.rb
+++
@@ -1,7 +0,0 @@
-require "spec_helper"
-
-describe "test cluster" do
-  it "starts on specs with elasticsearch: true", elasticsearch: true do
-    expect(Elasticsearch::Extensions::Test::Cluster.running?).to be true
-  end
-end
